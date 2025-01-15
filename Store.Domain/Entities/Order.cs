47eeee2e7d80df1e9cd2207ddf68756506545913
Silvio Sebastiany <silvio.sebastiany.gmail.com>
2025-01-14 23:46:49 -0300

    ✨ Validando o Pedido

--- Store.Domain/Entities/Order.cs
+++ Store.Domain/Entities/Order.cs
@@ -1,3 +1,5 @@
+using Flunt.Notifications;
+using Flunt.Validations;
 using Store.Domain.Enums;
 
 namespace Store.Domain.Entities
@@ -6,6 +8,12 @@ namespace Store.Domain.Entities
     {
         public Order(Customer customer, decimal deliveryFee, Discount discount)
         {
+            AddNotifications(new Contract<Notification>()
+                .Requires()
+                .IsNotNull(customer, "Customer", "Cliente inválido")
+                .IsGreaterThan(deliveryFee, 0, "DeliveryFee", "Taxa de entrega inválida")
+            );
+
             Customer = customer;
             Date = DateTime.Now;
             Number = Guid.NewGuid().ToString().Substring(0, 8);
@@ -26,7 +34,8 @@ namespace Store.Domain.Entities
         public void AddItem(Product product, int quantity)
         {
             var item = new OrderItem(product, quantity);
-            Items.Add(item);
+            if(item.IsValid)
+                Items.Add(item);
         }
 
         public decimal Total()
